f95105dbfbfcf877b927b4539db598438582c056
Chandan Chaurasia <cchaurasia@gmail.com>
2024-09-01 14:10:33 +0545

    pages added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -427,10 +427,6 @@
         }
         $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
     }
-    // AJAX MailChimp
-    $(".newsletter-form").ajaxChimp({
-        url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-        callback: callbackFunction
-    });
+
 
 })(jQuery);
